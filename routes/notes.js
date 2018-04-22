e5950ae003b4c9d9aeaed350a2e99e4a231be697
Seha Dalay <sehagulnar@gmail.com>
2018-04-22 13:00:17 -0400

    Update notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,105 +1,106 @@
 'use strict';
 
 const express = require('express');
-// Create an router instance (aka "mini-app")
-const router = express.Router();
 
 const mongoose = require('mongoose');
-const { MONGODB_URI } = require('../config');
 const Note = require('../models/note');
 
+const router = express.Router();
 /* ========== GET/READ ALL ITEM ========== */
 router.get('/notes', (req, res, next) => {
+    const searchTerm = req.query.searchTerm;
+    let filter = {};
 
-  const searchTerm = req.query.searchTerm;
-  let filter= {};
+    if (searchTerm) {
+      const re = new RegExp(searchTerm, 'i');
 
-  if(searchTerm){
-    const re = new RegExp(searchTerm, 'i');
-    filter.title = { $regex: re };
-  }
-  
-  return Note.find(filter)
-    .sort('created')
-    .then(results => {
-      res.json(results);
-    })
-    .catch(next);
+      filter.title = { $regex: re };
+      console.log(filter.title, 'FILTER‹‹‹‹‹‹‹‹‹‹‹‹‹‹››››››››››››››')
+    }
 
-//   res.json([
-//     { id: 1, title: 'Temp 1' }, 
-//     { id: 2, title: 'Temp 2' }, 
-//     { id: 3, title: 'Temp 3' }
-//   ]);
-
+    return Note.find(filter)
+      .sort('created')
+      .then(results => {
+        res.json(results);
+      })
+      .catch(next);
 });
 
 /* ========== GET/READ A SINGLE ITEM ========== */
 router.get('/notes/:id', (req, res, next) => {
-  const {id} = req.params.id;
-
-  return Note.findById(id)
-    .then(results => {
-      if (results) {
-        res.json(results);
-      } else{
-        next();
+    const id = req.params.id;
+    return Note.findById(id)
+      .then(result => {
+       if (result) {
+         res.json(result);
+       } else {
+        res.status(400).json({ message: 'The `id` is not valid' })
       }
     })
-    .catch(err =>{
-      next(err);
-    });
-
+      .catch(err => {
+        next(err);
+      });
 });
 
 /* ========== POST/CREATE AN ITEM ========== */
 router.post('/notes', (req, res, next) => {
+  const { title, content, id } = req.body;
 
-  const newItem= {
-    title: req.body.title,
-    content: req.body.content
+  const newNote = {
+    title: title,
+    content: content,
+    id: id
   };
 
-  return Note.create(newItem)
-    .then(result => {
-      res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
+  if (!req.body.title) {
+  const err = new Error('Missing `title` in request body');
+    err.status = 400;
+   return next(err);
+  }
+
+  return Note.create(newNote)
+    .then(results => {
+      res.location(`/api/notes/${id}`).status(201).json(results)
     })
-    .catch(next);
+    .catch(err => {
+      next(err);
+    });
 });
-
-
 
 /* ========== PUT/UPDATE A SINGLE ITEM ========== */
 router.put('/notes/:id', (req, res, next) => {
 
   const id = req.params.id;
-  const updatedItem = {
-    title: req.body.title,
-    content: req.body.content
+  const { title, content} = req.body;
+  const options = { new: true };
+  const updateNote = {
+    title: title,
+    content: content
   };
 
-  if (!updatedItem.title) {
-    const err = new Error('Missing `title` in request body.');
-    console.error(err);
-  }
-
-  return Note.findByIdAndUpdate(id, updatedItem, { new: true })
-    .then(results => {
-      res.json(results).status(200);
+   return Note.findByIdAndUpdate(id, updateNote, options)
+     .then(result => {
+      if (result) {
+        res.json(result);
+      } else {
+        next();
+      }
     })
-    .catch(next);
+    .catch(err => {
+      next(err);
+    });
 });
 
 /* ========== DELETE/REMOVE A SINGLE ITEM ========== */
 router.delete('/notes/:id', (req, res, next) => {
-
   const id = req.params.id;
-
   return Note.findByIdAndRemove(id)
-    .then(() => {
-      res.status(204).end();
-    })
-    .catch(next);
+      .then(results => {
+        res.status(204).end();
+      })
+      .catch(err => {
+        next(err);
+      });
 });
 
 module.exports = router;
